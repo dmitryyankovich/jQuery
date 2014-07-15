3083706c31eca22d7c8b6afcde8746eaad566c19
dmitryyankovich <dmitryyankovich37@gmail.com>
2014-07-16 00:34:07 +0300

    added lens style and  template of callback function

--- jQueryPlugin/jquery.imageZoom.js
+++ jQueryPlugin/jquery.imageZoom.js
@@ -1,6 +1,12 @@
 (function( $ ) {
     $.fn.imageZoom = function(options) {
+        var lensStyle = "background-position: 0px 0px;width: 100px;height: 100px;float: left;display: none;" +
+            "border-radius: 54px;float: left;display: none;border: 4px solid #888;background-repeat: no-repeat;" +
+            "position: absolute;";
 
+        return this.each(function () {
+            obj = $(this);
+        });
     }
 
 })(jQuery);
