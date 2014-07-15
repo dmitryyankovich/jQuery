f542be87879f6c20ac7e64b936b14ace1d92311a
dmitryyankovich <dmitryyankovich37@gmail.com>
2014-07-15 10:44:17 +0300

    added check for left scroll

--- inView/pseudoSelector.js
+++ inView/pseudoSelector.js
@@ -1,11 +1,15 @@
 $.expr[':'].inView = function(obj) {
     var windowScrollTop = $(window).scrollTop();
+    var windowScrollLeft = $(window).scrollLeft();
     var windowHeight = $(window).height();
+    var windowWidth = $(window).width();
     var element = $(obj);
     var offset = element.offset();
 
-    if( (element.innerHeight() + offset.top) > windowScrollTop && (offset.top - windowScrollTop) < windowHeight ){
-        return true;
+    if((element.innerHeight() + offset.top) > windowScrollTop && (offset.top - windowScrollTop) < windowHeight ) {
+        if ((element.innerWidth() + offset.left) > windowScrollLeft && (offset.left - windowScrollLeft) < windowWidth) {
+            return true;
+        }
     } else {
         return false;
     }
