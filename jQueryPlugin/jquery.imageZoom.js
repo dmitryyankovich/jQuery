95e8f2646d05804904e8716d7f13fcfb7a3b14dc
dmitryyankovich <dmitryyankovich37@gmail.com>
2014-07-16 08:51:35 +0300

    added options

--- jQueryPlugin/jquery.imageZoom.js
+++ jQueryPlugin/jquery.imageZoom.js
@@ -1,8 +1,18 @@
 (function( $ ) {
     $.fn.imageZoom = function(options) {
-        var lensStyle = "background-position: 0px 0px;width: 100px;height: 100px;float: left;display: none;" +
-            "border-radius: 54px;float: left;display: none;border: 4px solid #888;background-repeat: no-repeat;" +
-            "position: absolute;";
+
+        var settings = $.extend( {
+            lensSize: 100,
+            borderSize: 4,
+            lensCss : "lens",
+            borderColor: "#888"
+        }, options);
+
+        var lensStyle = "background-position: 0px 0px;width: " + String(settings.lensSize)
+            + "px;height: " + String(settings.lensSize) + "px;float: left;display: none;border-radius: "
+            + String(settings.lensSize / 2 + settings.borderSize)
+            + "px;float: left;display: none;border: " + String(settings.borderSize) + "px solid " + settings.borderColor
+            + ";background-repeat: no-repeat;position: absolute;";
 
         return this.each(function () {
             obj = $(this);
