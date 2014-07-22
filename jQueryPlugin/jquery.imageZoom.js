93feccf24f0ad1647dbdff77deb3e21b4944fbbe
dmitryyankovich <dmitryyankovich37@gmail.com>
2014-07-22 13:19:10 +0300

    reworked settings

--- jQueryPlugin/jquery.imageZoom.js
+++ jQueryPlugin/jquery.imageZoom.js
@@ -8,23 +8,18 @@
             + settings.borderSize + 'px solid ' + settings.borderColor
             + ';background-repeat: no-repeat;position: absolute;';
 
-        var obj, offset, lens, imageSrc, imageTag, widthRatio, heightRatio;
+        var obj, offset, lens, objectWidth, objectHeight, backgroundWidth, backgroundHeight;
 
         return this.each(function() {
             obj = $(this);
             offset = $(this).offset();
+            objectWidth = obj.width();
+            objectHeight = obj.height();
             lens = $('<div style="' + lensStyle + '"></div>').appendTo($('body'));
-            imageSrc = settings.imageSrc ? settings.imageSrc : $(this).attr('src');
-            imageTag = '<img style="display:none;" src="' + imageSrc + '">';
-            widthRatio = 0;
-            heightRatio = 0;
-
-            $(imageTag).load(function() {
-                widthRatio =  $(this).width() / obj.width();
-                heightRatio = $(this).height() / obj.height();
-            }).appendTo($(this).parent());
-            lens.css({ backgroundImage: "url('" + imageSrc + "')" });
-
+            backgroundWidth = objectWidth * settings.imgScale;
+            backgroundHeight = objectHeight * settings.imgScale;
+            lens.css({ backgroundImage: 'url("' + obj.attr('src') + '")',
+                "background-size": backgroundWidth + 'px' + ' ' + backgroundHeight + 'px'});
             lens.mousemove(changePosition);
             $(this).mousemove(changePosition);
             lens.mouseout(outOfImage);
@@ -32,14 +27,14 @@
         });
 
         function changePosition(e) {
-            var leftPosition = parseInt(e.pageX - offset.left);
-            var topPosition = parseInt(e.pageY - offset.top);
+            var leftPosition = e.pageX - offset.left;
+            var topPosition = e.pageY - offset.top;
 
             if (!(leftPosition < 0 || topPosition < 0 || leftPosition > obj.width() || topPosition > obj.height())) {
                 lens.show();
-                leftPosition = ((e.pageX - offset.left) * widthRatio - lens.width() / 2) * (-1);
-                topPosition = ((e.pageY - offset.top) * heightRatio - lens.height() / 2) * (-1);
-                lens.css({ backgroundPosition: leftPos + 'px ' + topPos + 'px' });
+                leftPosition = (leftPosition * settings.imgScale - lens.width() / 2) * (-1);
+                topPosition = (topPosition * settings.imgScale - lens.height() / 2) * (-1);
+                lens.css({ backgroundPosition: leftPosition + 'px ' + topPosition + 'px' });
                 leftPosition = e.pageX - lens.width() / 2;
                 topPosition = e.pageY - lens.height() / 2;
                 lens.css({ left: leftPosition + 'px', top: topPosition + 'px' });
@@ -55,7 +50,8 @@
         lensSize: 100,
         borderSize: 4,
         lensClass : 'lens',
-        borderColor: '#888'
+        borderColor: '#888',
+        imgScale: 2
     }
 
 })(jQuery);
